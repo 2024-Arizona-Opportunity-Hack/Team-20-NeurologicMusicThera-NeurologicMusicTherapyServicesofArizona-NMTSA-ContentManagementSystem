3c4e86b8b6d44602301d128a8c87833af2b414f7
Ayaan Panda <ayaan@ayaans-mbp.3520.dhcp.asu.edu>
2024-10-12 15:20:31 -0700

    frontend so far its really bad still working

--- nmtsa/app/admin-dashboard/page.tsx
+++ nmtsa/app/admin-dashboard/page.tsx
@@ -2,20 +2,37 @@ import { Navbar } from "@/components/Navbar";
 import { TableVideos } from "@/components/admin/TableVideos";
 
 export default function AdminDashboard() {
-    const arrayofvids = [
+    const videos = [
         {
-            title: "test",
-            filePath: "https://example",
-            transcript: "This is a transcript",
-            group: "Admin",
-            category: "Admin",
-            tags: ["Admin", "test"]
-        }
-    ]
+            title: 'Video 1',
+            tags: ['tag1', 'tag2'],
+            filepath: '/path/to/video1.mp4',
+            category: 'Category 1',
+            transcript: 'This is the transcript for video 1',
+            accessGroup: 'Group 1',
+        },
+        {
+            title: 'Video 2',
+            tags: ['tag3', 'tag4'],
+            filepath: '/path/to/video2.mp4',
+            category: 'Category 2',
+            transcript: 'This is the transcript for video 2',
+            accessGroup: 'Group 2',
+        },
+        {
+            title: 'Video 3',
+            tags: ['tag5', 'tag6'],
+            filepath: '/path/to/video3.mp4',
+            category: 'Category 3',
+            transcript: 'This is the transcript for video 3',
+            accessGroup: 'Group 3',
+        },
+    ];
+
     return (
         <main>
         <Navbar></Navbar>
-        <TableVideos name="videos" list={arrayofvids}></TableVideos>
+        <TableVideos></TableVideos>
         </main>
     );
 }
